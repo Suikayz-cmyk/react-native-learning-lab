45d59b0e922485d92dfeacf2921133fb47de032f
Muhammad Prayogo Pangestu <prayogoxyz@gmail.com>
2026-03-17 15:28:17 +0700

    feat(todo-state): add todo context with provider

--- apps/state-management-todo/App.js
+++ apps/state-management-todo/App.js
@@ -1,20 +1,17 @@
+// App.js
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { TodoProvider } from './src/context/TodoContext';
+import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ return (
+  <SafeAreaProvider>
+    <TodoProvider>
+      <StatusBar style='light' />
+      <HomeScreen />
+    </TodoProvider>
+  </SafeAreaProvider>
+ );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
